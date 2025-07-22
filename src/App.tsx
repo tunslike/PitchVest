441bbb52b0ecf4d3078c4bacafaecbd2b66df8df
tunslike <tunslike@yahoo.com>
2025-07-22 10:30:41 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h5 className='text-4xl text-blue-500'>Hello World</h5>
+    </>
+  )
+}
+
+export default App
